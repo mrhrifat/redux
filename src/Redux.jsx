f84a13790de1b58cdbcc9b9d70c6dcca98fda651
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-09 00:54:34 +0600

    Upload Root Components

---
+++ src/Redux.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Lesson1 from './Lesson1/Lesson1'
+
+class Redux extends Component {
+    render() {
+        return (
+            <div className='container'>
+                <h4 className='text-center'>Bismillahir Rahmanir Rahim</h4>
+                <Lesson1/>
+            </div>
+        )
+    }
+}
+
+export default Redux

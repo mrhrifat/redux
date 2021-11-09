caef55cebdccd14ad48d5b518179ea2453256e09
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-10 01:24:21 +0600

    Upload Lesson1 with Component

---
+++ src/Lesson1/Lesson1.jsx
@@ -0,0 +1,167 @@
+import React, { Component } from 'react'
+import { createStore } from 'redux'
+
+class Lesson1 extends Component {
+    render() {
+
+
+        // const reducer = (state = {}, action)=>{
+        //     return state
+        // }
+
+        // const store=createStore(reducer)
+
+        // store.subscribe(()=>{
+        //     console.log(store.getState())
+        // })
+
+        // store.dispatch({type:'Something'})
+
+        // const reducer = (state = {}, action) => {
+        //     if (action.type === 'A') {
+        //         return {
+        //             A: 'I am A'
+        //         }
+        //     }
+        //     return state
+        // }
+
+        // const store = createStore(reducer)
+
+        // store.subscribe(() => {
+        //     console.log(store.getState())
+        // })
+
+        // store.dispatch({ type: 'Something' })
+        // store.dispatch({ type: 'Something' })
+        // store.dispatch({ type: 'A' })
+        // store.dispatch({ type: 'Something' })
+
+
+        // const reducer = (state = {}, action) => {
+        //     if (action.type === 'A') {
+        //         return {
+        //             A: 'I am A'
+        //         }
+        //     }
+        //     if(action.type==='B'){
+        //         return {
+        //             B:'I am B'
+        //         }
+        //     }
+        //     return state
+        // }
+
+        // const store = createStore(reducer)
+
+        // store.subscribe(() => {
+        //     console.log(store.getState())
+        // })
+
+        // store.dispatch({ type: 'Something' })
+        // store.dispatch({ type: 'Something' })
+        // store.dispatch({ type: 'A' })
+        // store.dispatch({ type: 'B' })
+        // store.dispatch({ type: 'Someting' })
+        // store.dispatch({ type: 'A' })
+        // store.dispatch({ type: 'Something' })
+
+        // const reducer = (state = {}, action) => {
+        //     if (action.type === 'A') {
+        //         return {
+        //             ...state,
+        //             A: 'I am A'
+        //         }
+        //     }
+        //     if(action.type==='B'){
+        //         return {
+        //             B:'I am B'
+        //         }
+        //     }
+        //     return state
+        // }
+
+        // const store = createStore(reducer)
+
+        // store.subscribe(() => {
+        //     console.log(store.getState())
+        // })
+
+        // store.dispatch({ type: 'Something' })
+        // store.dispatch({ type: 'Something' })
+        // store.dispatch({ type: 'A' })
+        // store.dispatch({ type: 'B' })
+        // store.dispatch({ type: 'Someting' })
+        // store.dispatch({ type: 'A' })
+        // store.dispatch({ type: 'Something' })
+
+
+        // const reducer = (state = {}, action) => {
+        //     if (action.type === 'A') {
+        //         return {
+        //             A: 'I am A'
+        //         }
+        //     }
+        //     if(action.type==='B'){
+        //         return {
+        //             ...state,
+        //             B:'I am B'
+        //         }
+        //     }
+        //     return state
+        // }
+
+        // const store = createStore(reducer)
+
+        // store.subscribe(() => {
+        //     console.log(store.getState())
+        // })
+
+        // store.dispatch({ type: 'Something' })
+        // store.dispatch({ type: 'Something' })
+        // store.dispatch({ type: 'A' })
+        // store.dispatch({ type: 'B' })
+        // store.dispatch({ type: 'Someting' })
+        // store.dispatch({ type: 'A' })
+        // store.dispatch({ type: 'Something' })
+
+        const reducer = (state = {}, action) => {
+            if (action.type === 'A') {
+                return {
+                    ...state,
+                    A: 'I am A'
+                }
+            }
+            if(action.type==='B'){
+                return {
+                    ...state,
+                    B:'I am B'
+                }
+            }
+            return state
+        }
+
+        const store = createStore(reducer)
+
+        store.subscribe(() => {
+            console.log(store.getState())
+        })
+
+        store.dispatch({ type: 'Something' })
+        store.dispatch({ type: 'Something' })
+        store.dispatch({ type: 'A' })
+        store.dispatch({ type: 'B' })
+        store.dispatch({ type: 'Someting' })
+        store.dispatch({ type: 'A' })
+        store.dispatch({ type: 'Something' })
+
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Lesson1

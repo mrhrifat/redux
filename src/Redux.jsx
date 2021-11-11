b71ea01abe833bce412b94da1329b7f38028c2c6
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-11 19:47:24 +0600

    Update with Pro2.jsx Component Import

--- src/Redux.jsx
+++ src/Redux.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Pro from './Counter/Pro'
+import Pro2 from './Counter2/Pro2'
 import Lesson1 from './Lesson1/Lesson1'
 
 class Redux extends Component {
@@ -9,6 +10,8 @@ class Redux extends Component {
                 <h4 className='text-center'>Bismillahir Rahmanir Rahim</h4>
                 <Lesson1/>
                 <Pro/>
+                <div className="my-2"></div>
+                <Pro2/>
             </div>
         )
     }

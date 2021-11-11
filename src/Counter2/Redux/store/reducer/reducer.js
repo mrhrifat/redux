3bbdce7c9c0511403806207447e796c360adcf99
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-11 19:46:04 +0600

    Update Redux Store, Reducer, Actions

---
+++ src/Counter2/Redux/store/reducer/reducer.js
@@ -0,0 +1,21 @@
+const initCount = {
+    count: 0
+}
+const reducer = (state = initCount, action) => {
+    switch (action.type) {
+        case 'Increment': {
+            return {
+                count: state.count + 1
+            }
+        }
+        case 'Decrement': {
+            return {
+                count: state.count - 1
+            }
+        }
+        default: {
+            return initCount
+        }
+    }
+}
+export default reducer
